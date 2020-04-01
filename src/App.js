807dde6e984dbaf4d6f49faac316e3b3dafe3005
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-01 11:48:55 -0500

    class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import './App.css';
+import axios from 'axios'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    
+    fetch("http://anapioficeandfire.com/api/characters")
+      .then(response => response.json()) // first response => resolved
+      .then(
+        result => {
+          console.log(result.results)
+
+          this.setState({
+            isLoaded: true,
+            items:result.results
+          })
+
+
   render() {
     return (
       <div>
+        <h1> Game of Thrones API </h1>
         
       </div>
     )
