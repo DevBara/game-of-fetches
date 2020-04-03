f92a12e1463199f3a63a31e2b3c61c24a615b313
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-02 23:02:47 -0500

    updated components/axios Targaryen

--- src/App.js
+++ src/App.js
@@ -1,45 +1,95 @@
-import React from 'react'
+import React, { Component } from 'react'	
 import './App.css';
-import axios from 'axios'
-import JohnSnow from './components/JohnSnow.js'
-import Houses from './components/Houses.js'
+import MargaeryTyrell from './components/MargaeryTyrell'
+import HouseTargaryen from './components/HouseTargaryen'
+import HouseLannister from './components/HouseLannister'
+import HouseBaratheon from './components/HouseBaratheon'
+import RobertBaratheon from './components/RobertBaratheon'
+import JohnSnow from './components/JohnSnow'
+import HouseStark from './components/HouseStark'
+import CatelynStark from './components/CatelynStark'
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
+	
+	export default class App extends Component {
+		constructor(props) {
+		super(props);
+	
+			this.state = {
+			isLoaded: false,
+			data: []
+	}
+}
+	
+	render() {
+		return (
+			<div>
+				<h1> Game Of Thrones Characters </h1>
+				<JohnSnow />
+				<MargaeryTyrell />
+				<HouseTargaryen />
+				<HouseLannister />
+				<HouseBaratheon />
+				<RobertBaratheon />
+				<HouseStark />
+				<CatelynStark />
+			</div>
+		)
+	}
+}
 
-    this.state ={
-      characterData: null,
-      houseData:null,
-    };
-  }
 
-  async getHouses() {
-    try {
-      const abuelo = await axios.get('https://www.anapioficeandfire.com/api/houses');
-      this.setState({ houses: whatever.data });
-    } catch(e) {
-      console.error(e);
-    }
-  }
 
-  async componentDidMount(){
-    const res= await axios.get ('https://anapioficeandfire.com/api/characters/583');
-    this.setState({characterData: res.data})
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+
+//     this.state ={
+//       characterData: null,
+//       houseData:null,
+//     };
+//   }
+
+//   async getHouses() {
+//     try {
+//       const abuelo = await axios.get('https://www.anapioficeandfire.com/api/houses');
+//       this.setState({ houses: whatever.data });
+//     } catch(e) {
+//       console.error(e);
+//     }
+//   }
+
+//   async componentDidMount(){
+//     const res= await axios.get ('https://anapioficeandfire.com/api/characters/583');
+//     this.setState({characterData: res.data})
+//   }
 
  
 
-render(){
-  return(
-    <div className="app">
-      <header className="App-header">
-        <JohnSnow info={this.state.characterData} />
-      </header>
-    </div>
-  )
-}
-}
+// render(){
+//   return(
+//     <div className="app">
+//       <header className="App-header">
+//         <JohnSnow info={this.state.characterData} />
+//       </header>
+//     </div>
+//   )
+// }
+// }
 
 
-export default App
+// export default App
